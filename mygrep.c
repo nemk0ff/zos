4abaaf030e16b0fcb6304338b02e846ff98ad37b
Nemkoff <nemkov_work@mail.ru>
2025-09-24 22:51:45 +0300

    добавил функцию поиска pattern_in_line

--- mygrep.c
+++ mygrep.c
@@ -4,6 +4,21 @@
 #include <stdbool.h>
 #include <string.h>
 
+typedef struct {
+    char *data;
+    size_t capacity;
+    size_t size;
+} String;
+
+bool pattern_in_line(const char* pattern, const String* line) {
+    size_t pattern_length = strlen(pattern);
+    if (pattern_length > line->size) return false;
+    for (size_t i = 0; i < line->size - pattern_length; i++) {
+        if (strncmp(pattern, &(line->data[i]), pattern_length) == 0) return true;
+    }
+    return false;
+}
+
 int main(int argc, char **argv) {
     if (argc < 2) {
         fprintf(stderr, "Too few arguments\n");
